28b84154e7b94ff1aa9099551f22677ed35bf53f
archefnx <artykbaiev0000@gmail.com>
2026-04-05 15:57:18 +0500

    Сортировка выбором

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	arr := []int{5, 1, 1, 2, 0, 0}
 	//res := sorts.MergeSort(arr)
 	//res := sorts.QuickSort(arr, 0, len(arr)-1)
-	res := sorts.InsertionSort(arr)
+	//res := sorts.InsertionSort(arr)
+	res := sorts.SelectionSort(arr)
 	fmt.Println(res)
 }
---
+++ sorts/selection_sort.go
@@ -0,0 +1,19 @@
+package sorts
+
+func SelectionSort(arr []int) []int {
+	for i := 0; i < len(arr)-1; i++ {
+		min := i
+
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[min] {
+				min = j
+			}
+		}
+
+		if min != i {
+			arr[i], arr[min] = arr[min], arr[i]
+		}
+	}
+
+	return arr
+}
