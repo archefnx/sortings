c7ba451a4d24227699092d7f5fde3274ba78013d
archefnx <artykbaiev0000@gmail.com>
2026-04-05 15:56:14 +0500

    Сортировка вставками

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	//arr := []int{5, 2, 3, 1}
 	arr := []int{5, 1, 1, 2, 0, 0}
 	//res := sorts.MergeSort(arr)
-	res := sorts.QuickSort(arr, 0, len(arr)-1)
+	//res := sorts.QuickSort(arr, 0, len(arr)-1)
+	res := sorts.InsertionSort(arr)
 	fmt.Println(res)
 }
---
+++ sorts/insertion_sort.go
@@ -0,0 +1,14 @@
+package sorts
+
+func InsertionSort(arr []int) []int {
+	for i := 1; i < len(arr); i++ {
+		j := i
+
+		for j > 0 && arr[j-1] > arr[j] {
+			arr[j], arr[j-1] = arr[j-1], arr[j]
+			j--
+		}
+	}
+
+	return arr
+}
